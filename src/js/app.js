97cbb533365f39fee2911faf70573f2924f8a0ce
Andrey <ckastrusshion@gmail.com>
2024-03-06 14:17:51 +0300

    add tooltip and date

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 
-import { addEventOnElement } from "./utils.js";
+import { addEventOnElement, getGreetingMsg } from "./utils.js";
+import { Tooltip } from "./components/Tooltip.js";
 
 /**
  * Toogle sidebar in small screen
@@ -14,4 +15,35 @@ const $overlay = document.querySelector("[data-sidebar-overlay]");
 addEventOnElement($sidebarTogglers, "click", function () {
     $sidebar.classList.toggle("active");
     $overlay.classList.toString("active");
-})
+});
+
+
+/**
+ * Initialize tooltip behavior for all DOM elements with a "data-tooltip" attribute.
+ */
+
+const tooltipElems = document.querySelectorAll("[data-tooltip]");
+
+tooltipElems.forEach(elem => Tooltip(elem));
+
+
+
+/**
+ * Show greeting message on homepage
+ */
+
+const $greetElem = document.querySelector("[data-greeting]");
+const currentHour = new Date().getHours();
+$greetElem.textContent = getGreetingMsg(currentHour);
+
+
+/**
+ * Show current date on homepage
+ */
+
+const $currentDataElem = document.querySelector("[data-current-date]");
+$currentDataElem.textContent = new Date().toDateString().replace(``, ``);
+
+/**
+ * Notebook crate field
+ */
--- src/js/utils.js
+++ src/js/utils.js
@@ -6,7 +6,20 @@ const addEventOnElement = function ($elements, eventType, callback) {
     $elements.forEach($element => $element.addEventListener(eventType, callback));
 }
 
+const getGreetingMsg = function (currentHour) {
+    const greeting = currentHour < 5 ? "Night" :
+        currentHour < 12 ? "Morning" :
+            currentHour < 15 ? "Noon" :
+                currentHour < 17 ? "Afternoob" :
+                    currentHour < 20 ? "Evening" :
+                        "Night";
+
+
+    return `Good ${greeting}`;
+}
+
 
 export {
-    addEventOnElement
+    addEventOnElement,
+    getGreetingMsg
 }
